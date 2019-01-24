34b21917f136dc6078b601d8771711b94618db98
Gabew19 <47000024+Gabew19@users.noreply.github.com>
2019-01-24 13:43:08 -0500

    Added RGB

--- welcome/welcome.lua
+++ welcome/welcome.lua
@@ -2,9 +2,9 @@ firstSpawn = true
 
 AddEventHandler("playerSpawned", function(spawn)
     if(firstSpawn) then
-        TriggerEvent("chatMessage", "", { 0, 0, 0 }, "Welcome to West California RP!")
-		TriggerEvent("chatMessage", "", { 0, 0, 0 }, "TeamSpeak: 134.255.231.69:9031")
-		TriggerEvent("chatMessage", "", { 0, 0, 0 }, "Discord: https://discord.gg/6cKhEAj")
+        TriggerEvent("chatMessage", "", { 255, 255, 255 }, "Welcome to West California RP!")
+		TriggerEvent("chatMessage", "", { 0, 128, 128 }, "TeamSpeak: 134.255.231.69:9031")
+		TriggerEvent("chatMessage", "", { 0, 128, 128 }, "Discord: https://discord.gg/6cKhEAj")
         firstSpawn = false
     end
 end)
